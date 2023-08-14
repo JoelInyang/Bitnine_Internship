36228923c3a9325d995d81a88a36cad59961bb6e
Joel Inyang <joelinyang2003@gmail.com>
2023-08-14 20:26:37 +0100

    Bitnine Internship

--- Question_1/fibonacci.c
+++ Question_1/fibonacci.c
@@ -1,76 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum Operation {
+
+typedef enum TypeTag{
     ADD,
     SUB,
     MUL,
     DIV
-} Operation;
+    }TypeTag; //TypeTag contains enumerations of operations involved
 
-typedef struct ExpressionNode {
-    Operation op;
-    union {
+typedef struct Node{ // Node is a struct that contains multiple data types which are "TypeTag" and "Union"
+    TypeTag type;
+    union { // the union construct makes it possible for the program to run either and int or a struct but if contains operations will run any of the operations involved
         int value;
         struct {
-            struct ExpressionNode* left;
-            struct ExpressionNode* right;
-        } operands;
-    } data;
-} ExpressionNode;
-
-ExpressionNode* createNode(Operation op) {
-    ExpressionNode* newNode = (ExpressionNode*)malloc(sizeof(ExpressionNode));
-    newNode->op = op;
+            struct Node* left; // Left on the expression tree/calculation sides
+            structNod* right; // Right on the e expression tree/calculation sides
+        }operands; // The name of this particular structure
+    }data;
+}Node;
+Node* createNode(TypeTag type) { //this declares a function and takes TypeTag(which contains the operations) as an argument and return a pointer to the node struct
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->type = type;
     newNode->data.operands.left = NULL;
     newNode->data.operands.right = NULL;
     return newNode;
 }
 
-void calculate(ExpressionNode* node) {
-    if (node == NULL) {
+void calculate(Node* node) { //this returns a void(nothing), it takes a node argument which returns a pointer to the Node structure
+    if (node == NULL) { //if the node is null return nothing which means the node doesnt have an operation value
         return;
     }
-
-    switch (node->op) {
+    //node here refers to an expression denotes an operation(addition or subtraction) or a value(like numbers)
+    switch (node->type) {
         case ADD:
-            calculate(node->data.operands.left);
+            calculate(node->data.operands.left); //calculate here is a recursive function(that is a function that calls itself)
             calculate(node->data.operands.right);
-            node->data.value = node->data.operands.left->data.value + node->data.operands.right->data.value;
+            node->data.value = node->data.operands.left->data.value + node ->data.operands.right->data.value; //getting the left value of the operation and getting the right value of the operation then performing the ADD operation then assigning it to the node value.
             break;
         case SUB:
             calculate(node->data.operands.left);
             calculate(node->data.operands.right);
-            node->data.value = node->data.operands.left->data.value - node->data.operands.right->data.value;
+            node->data.value = node->data.operands.left->data.value - node->data.operands.right->data.value; //getting the left value of the operation and getting the right value of the operation then performing the SUB operation then assigning it to the node value.
             break;
         case MUL:
             calculate(node->data.operands.left);
             calculate(node->data.operands.right);
-            node->data.value = node->data.operands.left->data.value * node->data.operands.right->data.value;
+            node->data.value = node->data.operands.left->data.value * node->data.operands.right->data.value; //getting the left value of the operation and getting the right value of the operation then performing the MUL operation then assigning it to the node value.
             break;
         case DIV:
             calculate(node->data.operands.left);
             calculate(node->data.operands.right);
-            node->data.value = node->data.operands.left->data.value / node->data.operands.right->data.value;
+            node->data.value = node->data.operands.left->data.value / node->data.operands.right->data.value; //getting the left value of the operation and getting the right value of the operation then performing the DIV operation then assigning it to the node value.
             break;
     }
 }
 
-int main() {
-    ExpressionNode* add = createNode(ADD);
-    add->data.operands.left = createNode(ADD);
-    add->data.operands.left->data.operands.left = createNode(ADD);
-    add->data.operands.left->data.operands.left->data.value = 1;
-    add->data.operands.left->data.operands.right = createNode(ADD);
-    add->data.operands.left->data.operands.right->data.value = 1;
+
+int main() { //the main function is the entry point of every C program
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    Node* add = createNode(ADD); //am using the createNode function i defined earlier to create a new node, using the Node structure defined earlier too and assigning to a pointer variable add
+    add->data.operands.left = createNode(ADD); // creates a new node in the left operand of the data union, where this new node now contains it own left and right operands as well, making it a nested node still using the data struct
+    add->data.operands.left->data.operands.left = createNode(ADD); //creates a new ADD node and assign it to the nested left operand(that is the left operand of the left operand of the add node)
+    add->data.operands.left->data.operands.left->data.value = num1; // assigning the value of the left operand of the left operand to 1
+    add->data.operands.left->data.operands.right = createNode(ADD); // creates a new node and assign it to the right operand of the left operand of the add node
+    add->data.operands.left->data.operands.right->data.value = num2; //assigning the value of the right operand of the left operand of the add variable to 1
     add->data.operands.right = createNode(ADD);
     add->data.operands.right->data.operands.left = createNode(ADD);
-    add->data.operands.right->data.operands.left->data.value = 1;
+    add->data.operands.right->data.operands.left->data.value = num1;
     add->data.operands.right->data.operands.right = createNode(ADD);
-    add->data.operands.right->data.operands.right->data.value = 0;
+    add->data.operands.right->data.operands.right->data.value = num2;
 
-    ExpressionNode* mul = createNode(MUL);
-    mul->data.operands.left = createNode(MUL);
+
+
+    Node* mul = createNode(MUL); // am using the createNode function i defined earlier to create a new node, using the Node structure defined earlier too and assigning to a pointer to a variable called mul
+    mul->data.operands.left = createNode(MUL); // creates a new node in the left operand of the data union, where  this new node now contains it own left and right operands as well
     mul->data.operands.left->data.operands.left = createNode(MUL);
     mul->data.operands.left->data.operands.left->data.value = 1;
     mul->data.operands.left->data.operands.right = createNode(MUL);
@@ -81,23 +92,27 @@ int main() {
     mul->data.operands.right->data.operands.right = createNode(MUL);
     mul->data.operands.right->data.operands.right->data.value = 0;
 
-    ExpressionNode* sub = createNode(SUB);
+    Node* sub = createNode(SUB);
     sub->data.operands.left = mul;
     sub->data.operands.right = add;
 
-    ExpressionNode* fibo = createNode(SUB);
+    Node* fibo = createNode(SUB);
     fibo->data.operands.left = sub;
     fibo->data.operands.right = NULL;
 
-    calculate(add);
-    calculate(mul);
-    calculate(sub);
-    calculate(fibo);
+    calculate(add); // calculates the values of the nodes using the calculate function i created earlier
+    calculate(mul); // calculates the values of the nodes using the calculate function i created earlier
+    calculate(sub); // calculates the values of the nodes using the calculate function i created earlier
+    calculate(fibo); // calculates the values of the nodes using the calculate function i created earlier
 
-    printf("add : %d\n", add->data.value);
-    printf("mul : %d\n", mul->data.value);
-    printf("sub : %d\n", sub->data.value);
-    printf("fibo : %d\n", fibo->data.value);
+
+
+
+
+    printf("add : %d\n", add->data.value); // prints the value
+    printf("mul : %d\n", mul->data.value); // prints the value
+    printf("sub : %d\n", sub->data.value); // prints the value
+    printf("fibo : %d\n", fibo->data.value); // prints the value
 
     free(add);
     free(mul);
@@ -106,4 +121,3 @@ int main() {
 
     return 0;
 }
-
--- Question_2/piecewise.c
+++ Question_2/piecewise.c
@@ -1,93 +1,93 @@
 #include <stdio.h>
 
-//First Method : Recursive Approach
-int recursivePiecewise(int n) {
-    if (n <= 2) {
+//First method: Recursive approach
+int recursivePiecewise(int n) { // a recursive function that takes n as a parameter and return an interger
+    if (n<=2) { //am saying if n<=2 then return n, because we already have the values defines that is F(0)=0, F(1)=1, F(2) = 2
         return n;
     }
-    return recursivePiecewise(n - 3) + recursivePiecewise(n - 2);
+    return recursivePiecewise(n -3) + recursivePiecewise(n-2); // but if any n value is greater than 2, then the recursivePiecewise function is recursively called and performs the calculation based  on [ F(n) = F(n-3) + F(n-2) ]
 }
 
 /*
-Advantages of Recursive approach:
---Reflects the recurrence relation directly
---The implementation is uncomplicated
+Advantages of recursive approach
+-- Much simpler and easier to calculate the output and straight forward to understand.
+-- For recursive approach the implementations are clear and uncomplicated as oppose to other methods
 
-Disadvantages of Recursive approach:
---There would be exponential time complexity
---Overlapping might occur due to large values of n
+Disadvantages of recursive approach
+-- There would be exponential time complexity that is as more input is added or increased by 1, it causes the number of operations to double.
+-- Overlapping might occur due to large values of n, so the more n increases the more the efficiency of this recursive method reduces.
 */
 
+//Second method: Memoization( Top-Down dynamic programming, it involves storing already calculated data in cache, avoid redundancy and it reads from top - bottom
+int memo[50];   //assuming n<=50
 
-//Second Method :  Memoization (Top-Down Dynamic Programming)
-int memo[50]; // Assuming n <= 50
-
-int memoizedPiecewise(int n) {
-    if (n <= 2) {
+int memoizedPiecewise(int n) {  //this is a memoized function that avoids redundancy by check if the value of n is already present/computed in the array above first before creating a new value in the memo array above.
+    if (n<=2) { //this checks if n is less than or equal to 2 as we already know/have the values for 0,1 and 2
         return n;
     }
-    if (memo[n] != -1) {
+    if (memo[n] != -1){ //this checks if the n value of memo is equal to -1(which means that "no value"), if it is not equal to -1 then there is a value, so i just return n
         return memo[n];
     }
-    memo[n] = memoizedPiecewise(n - 3) + memoizedPiecewise(n - 2);
+    memo[n] = memoizedPiecewise(n-3) + memoizedPiecewise(n-2); //else then, memoized is called recursively to calculate the value for n
     return memo[n];
 }
 
 /*
-Advantages of Memoization:
---The time complexity is reduced (depending on the size of n)
---The memo array above stores computed values thereby leading to no redundant calculations
+Advantages of memoization approach:
+--Unlike the pure recursive method the time complexity is reduced although still depends on the size of n
+--The memo array above stores computed values thereby leading to no redundant calculations.
 
-Disadvantages of Memoization:
---Function call overhead is still present
---Additional memory for the memoization array is required
+Disadvantages of memoization approach:
+--If there is need for an increase in the array, then additional memory is required
+--Function call is still present that is even tho its not recalculating values, process of computing values and returning the stored values still can add up and lead to function call overhead
 */
 
+//Third method : Iterative(Bottom-Up Dynamic programming which ) which just iterate through and uses a for loop as well to get the n value
 
-//Third Method : Iterative(Bottom-Up Dynamic Programming)
-int iterativePiecewise(int n) {
-    if (n <= 2) {
+int iterativePiecewise(int n){ // takes integer n as a parameter and returns an integer value
+    if(n<=2) { // since I already have F(0), F(1) and F(2) defined then i just return n
         return n;
     }
-    int dp[n + 1];
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
-        dp[i] = dp[i - 3] + dp[i - 2];
+    int F[n + 1]; // declares an integer array to store the computed values of F(i) for i ranging from 0 to n.
+    F[0] = 0; //base cases of the recurrence are defined already
+    F[1] = 1;
+    F10[2] = 2;
+    for (int i = 3; i <= n; i++) { // a for loop to loop through the values in the array from i =3 till it gets to n
+        F[i] = F[i -3] + F[i -2]; // F[i] here calculates the value of F(i) by summing up the previously calculated F(i-3) and F(i-2) from the F array
     }
-    return dp[n];
-}
-
-
+    return F[n]; // after the loop get to the n value which is the end of the loop then it returns the calculated value of F(n)
+    }
 
 /*
-Advantages of Iterative Approach:
--- Additional memory overhead is required here
---Computes values interatively and is efficient thereby avoiding function call overhead 
+Advantages of the iterative approach:
+-- This iterative approach computes the values of F(i) in a bottom-up manner, starting from base cases and building up to the vale of F(n). This approach avoids function call bottlenecks where each function call involves additional time and resources, but it rather involves simple arithmetic operations.
+-- Time complexity of calculation minimized since it involves simple arithmetic operations.
 
-Disadvantages of Iterative Approach:
---It may not be as inherent as the recursive approach.
---An array of size "n+1" is required to store intermediate values.
+Disadvantages of the iterative appraoch:
+-- It may not be as inherent as the recursive approach, or better said needs more understanding to be able to work with.
+-- An array of size "n+1" is required to store values from 0 to n, and this introduces the need for additional memory.
+
 */
+int main() { //the main function is the entry point of all C progarm
+    int n; //declaring a variable called n
 
 
-
-int main() { //main() the entry point of this C program
-    int n;
-
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
+    printf("Enter the value of n: "); //This accepts the value of n to be calculated from user
+    scanf("%d", &n); // storing the input of the user in the n variable declared earlier
 
     printf("Method 1: Recursive\n");
-    printf("F(%d) = %d\n\n", n, recursivePiecewise(n));
+    printf("F(%d) = %d\n\n", n, recursivePiecewise(n)); //This prints out the value of the n using the recursive approach
 
-    for (int i = 0; i < 50; i++) memo[i] = -1;
+    for (int i=0; i<50; i++) memo[i] = -1;  // The 50 here is based on the assumption i made while declaring the memo array in the method.
     printf("Method 2: Memoization\n");
-    printf("F(%d) = %d\n\n", n, memoizedPiecewise(n));
+    printf("F(%d) = %d\n\n", n, memoizedPiecewise(n)); //This prints out the value of the n using the  approach
 
     printf("Method 3: Iterative\n");
-    printf("F(%d) = %d\n", n, iterativePiecewise(n));
+    printf("F(d) = %d\n", n, iterativePiecewise(n)); //This prints out the value of the n using the iterative approach
 
     return 0;
 }
+
+
+
+
